d65453146966b3aa78f8beabe710a64908ecde0a
byq77 <byq77@hotmail.com>
2021-10-18 18:24:21 +0200

    learn workflows

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,79 @@
-#include <mbed.h>
+#include "mbed.h"
+#include "ros.h"
+#include "std_msgs/Float32.h"
 
-static const uint8_t led_mask[] = {
-    0,0b00000001,0b00000010,0b00000100,0b00000010,
-    0b00000001,0,0b00000111,0b00000101,0b00000010
+#define SPIN_DELAY_MS 10U
+#define SENSOR_INTER_MEASUREMENT_MS 1000U
+#define MOCK_PIN ADC_TEMP
+
+class RosMockSensorTest : public ros::NodeHandle, private NonCopyable<RosMockSensorTest>
+{
+public:
+    /**
+     * @brief Initialise ros mock sensor node.
+     */
+    void init()
+    {
+        // initialise ros node
+        initNode();
+
+        // prepare sensor data
+        sensor_msg.data = 0.0f;
+
+        // advertise publisher
+        advertise(sensor_pub);
+
+        // set LEDs states
+        led1 = 0;
+        led2 = 1;
+    };
+
+    /**
+     * @brief Run ros mock sensor node test.
+     */
+    void spin()
+    {
+        uint64_t last_publish_time = 0;
+
+        while (1)
+        {
+            uint64_t time = Kernel::get_ms_count();
+
+            if (time - last_publish_time >= SENSOR_INTER_MEASUREMENT_MS)
+            {
+                // signal with LEDs
+                led1 = !led1;
+                led2 = !led2;
+
+                // read mock sensor data
+                sensor_msg.data = mock_sensor.read();
+
+                // write log message
+                loginfo("Publishing a new sensor reading...");
+
+                // publish ros message
+                sensor_pub.publish(&sensor_msg);
+
+                last_publish_time = time;
+            }
+
+            spinOnce();
+
+            ThisThread::sleep_until(time + SPIN_DELAY_MS);
+        }
+    }
+
+private:
+    DigitalOut led1{LED1, 0};
+    DigitalOut led2{LED2, 0};
+    AnalogIn mock_sensor{MOCK_PIN};
+    std_msgs::Float32 sensor_msg;
+    ros::Publisher sensor_pub{"mock_sensor", &sensor_msg};
 };
-
-BusOut leds(LED1,LED2,LED3);
 
 int main()
 {
-    int k = sizeof(led_mask), n = 0;
-    while(1)
-    {
-        printf("Hello World from CORE2!\r\n");
-        leds = led_mask[n++ % k];
-        ThisThread::sleep_for(1000);
-    }
+    RosMockSensorTest mock_sensor;
+    mock_sensor.init();
+    mock_sensor.spin();
 }
